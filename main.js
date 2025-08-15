cb5764838813d304e9877f78963710e392e929c9
Amirali416 <bazrgar20@gmail.com>
2025-08-16 03:20:29 +0330

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-    const background = document.querySelector('.background');
-    const x = (window.innerWidth - e.pageX * 2) / 100;
-    const y = (window.innerHeight - e.pageY * 2) / 100;
-    background.style.transform = `translateX(${x}px) translateY(${y}px)`;
-});
